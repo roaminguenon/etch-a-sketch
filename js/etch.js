b7110c9c6dc34288332a69d107ba2a4745bf3c6c
roaminguenon <roaminguenon@gmail.com>
2017-04-30 09:45:24 -0700

    change jquery link to webpage

--- js/etch.js
+++ js/etch.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
-	//$("div#container").append($("<div class="grid"></div>"));
-	$("div#container").add($("<p>hellooo</p>"));
+	$("div#container").append($("<div class="grid"></div>"));
+	//$("div#container").add($("<p>hellooo</p>"));
+	console.log("yo")
 })
