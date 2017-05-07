6123527cefd8702675e9a4cbba72e4df4ec089ed
roaminguenon <roaminguenon@gmail.com>
2017-05-07 15:43:30 -0700

    add hover function, turn squares blue

--- js/etch.js
+++ js/etch.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
-	for(var i = 0; i < 16; i++ ) {
+	for(var i = 0; i < 256; i++ ) {
 		$("#container").append('<div class="grid"></div>');
 	}
-	
+	$('.grid').hover(function(){
+		$(this).css("background-color", "blue")
+	});
 
 })
+
