d6b3ab8a7b577b399a325680bcd53414b424df29
roaminguenon <roaminguenon@gmail.com>
2017-05-07 19:44:06 -0700

    everything works accept white space above squares

--- js/etch.js
+++ js/etch.js
@@ -1,11 +1,75 @@
-$(document).ready(function() {
 
-	for(var i = 0; i < 256; i++ ) {
-		$("#container").append('<div class="grid"></div>');
-	}
+$(document).ready(function() {
+	var x = 16;
+	
+	var gridNum = 256;
+
+	console.log(gridNum);
+
+	/*function removeGrid(className) {
+		$('className').remove();
+	};*/
+
+	makeGrid1();
+
+	//change squares to blue when hovered
+	
+
 	$('.grid').hover(function(){
-		$(this).css("background-color", "blue")
+		$(this).css("background-color", "blue");
 	});
 
-})
+	
+	function removeGrid() {
+						$('.grid').remove();
+						console.log('grid removed');
+	};
+
+	var newSize = 0;
+	function getGridSize() {
+		var gridSize = prompt("What grid size do you want?", "16");
+
+		if (gridSize != null) {
+			newSize = 600/gridSize;
+			console.log(newSize + "is the new width and height");
+
+
+		//	$('.grid').css("width", newSize);
+		//	$('.grid').css("height", newSize);
+			x = gridSize;
+			gridNum = Math.pow(x, 2);
+
+		};
+	};
+
+	function makeGrid1() {
+		console.log("makingGrid now");
+		for(var i = 0; i < gridNum; i++ ) {
+			$("#container").append('<div class="grid"></div>');
+			//console.log(i);
+			$('.grid').css("width", newSize);
+			$('.grid').css("height", newSize);
+		};
+		
+	};
+	//when button clicked reset squares to white and ask for new x
+	$('#button').click(function() {
+		console.log("button clicked");
+
+		//$('.grid').css("background-color", "white");
+
+		removeGrid();
+		
+		getGridSize();
+
+		makeGrid1();
+
+		$('.grid').hover(function(){
+			$(this).css("background-color", "blue");
+			console.log("hovered");
+		});
+		
+	});
+
+});
 
